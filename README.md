31b5c74fb2266d28b157d89a958bba34c5a50e4f
Daniel Rodriguez Baumann <daniel@triopsi.com>
2019-12-27 02:07:47 +0100

    Script hinzugefügt und Beschreibung verbessert

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # gsales_cron
-Automatsiertes erstellen von Rechnungen
+Automatsiertes erstellen und Versenden von Rechnungen per Gsales 2 API.
+
+## Vorrausetzung
+- \> PHP 5.4
+- Berechtigung, um ein Cronjob zu erstellen
+- Gsales User mit API berechtigung
+- Kunden haben die Option "Rechnung/Mahnung automatisch spoolen" aktiviert
+
+## Installation
+Git Repository klonen oder downloaden und die gsales_cron.php ausführbar machen. (0755)
+gsales_cron.php bearbeiten und `<API-Keys>` und `<Gsales-Domain>` durch eigene Werte ersetzen.
+## Cron
+Cron am besten per /etc/crontab ausführen lassen. Dazu bearbeiten wir die crontab
+```
+nano /etc/crontab
+```
+oder
+```
+crontab -e
+```
+Beispiel für die Ausführung jeden Tag um 0 Uhr:
+```
+0 0 * * * /usr/bin/php /path/to/gsales_cron.php
+```
+Beispiel für die Ausführung jede Stunde:
+```
+0 * * * * /usr/bin/php /path/to/gsales_cron.php
+```
+
+### Mehr Scripte/Unterstüzung
+- :orange_book: [Profoxi Blog](https://wiki.profoxi.de/)
---
+++ gsales_cron.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Automatsiertes erstellen und Versenden von Rechnungen per Gsales 2 API
+ * 
+ */
+ini_set("soap.wsdl_cache_enabled", "0"); 
+
+$strAPIKey = '<API-Keys>'; 
+$strApiWsdlUrl = 'http://<Gsales-Domain>/gsales2/api/api.php?wsdl'; 
+
+$client = new soapclient($strApiWsdlUrl);  
+$arrCount = $client->processContractsRepayable($strAPIKey);
+if ($arrCount['result'] > 0){
+    $arrayOfInvoices = $client->createInvoicesFromQueueForAll($strAPIKey); 
+    foreach ((array)$arrayOfInvoices['result'] as $key => $objInvoice){ 
+        $arrFilterMailspool[] = array('field'=>'sub_recordset_id', 'operator'=>'is', 'value'=>$objInvoice->base->id); 
+        $arrFilterMailspool[] = array('field'=>'sub', 'operator'=>'is', 'value'=>'subinvoice'); 
+        $arrSortMailspool = array('field'=>'created', 'direction'=>'asc'); 
+        $arrMailspoolEntries = $client->getMailspoolEntries($strAPIKey, $arrFilterMailspool, $arrSortMailspool,999,0); 
+        if (isset($arrMailspoolEntries['result']['0'])){ 
+            $client->setMailspoolSendApprovalForEntry($strAPIKey, $arrMailspoolEntries['result']['0']->id); 
+            unset($arrFilterMailspool, $arrSortMailspool, $arrMailspoolEntries); 
+        }
+    } 
+    //Mailpool Absenden
+    $client->sendMailspool($strAPIKey); 
+} 
